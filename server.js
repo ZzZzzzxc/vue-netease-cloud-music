e7d813aff829be330d098338c63be3eeac5b885b
ZxcCC <731458684@qq.com>
2020-07-31 18:07:17 +0800

    feat: node server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const compression = require('compression');
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(compression())
+app.use("/", express.static(__dirname + "/dist"));
+app.listen(5001, () => console.log("http://localhost:5001"));
